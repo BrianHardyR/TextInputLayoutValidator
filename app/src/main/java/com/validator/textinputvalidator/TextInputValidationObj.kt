870b3818f611bd463834dbe95e2c57afebf42ade
brian <elibrianga@gmail.com>
2021-07-06 21:41:44 +0300

    Some house cleaning

---
+++ app/src/main/java/com/validator/textinputvalidator/TextInputValidationObj.kt
@@ -0,0 +1,13 @@
+package com.validator.textinputvalidator
+
+/**
+ * @param default the string shown on the [TextInputEditText] before data is entered
+ * @param error the string shown as error text
+ * @param validators collection of validators
+ *          @example listOf( { text ->  text.isNullOrEmpty() } ... )
+ */
+sealed class TextInputValidationObj(
+    val default: String = "",
+    val validators: Collection<(String) -> Boolean> = emptyList(),
+    val error: String = "",
+)
---
+++ app/src/main/java/com/validator/textinputvalidator/TextInputValidator.kt
@@ -0,0 +1,62 @@
+package com.validator.textinputvalidator
+
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
+
+
+/**
+ * This function validates the input of a [TextInputLayout]'s EditText against a list of validators
+ * if the input violates any validator the 'error' message is shown
+ * otherwise the onValid callback is triggered
+ */
+
+fun TextInputLayout.validate(
+    default: String = "",
+    validators: Collection<(String) -> Boolean> = emptyList(),
+    error: String = "",
+    onValid: (String) -> Unit
+) {
+    if (editText == null) throw IllegalArgumentException("Validation requires at least one TextInputEditText as a child")
+    val validationObj = TextInputValidationObj(default, validators, error)
+    setTag(R.id.validationTag, validationObj)
+    editText?.setText(default)
+    editText?.doOnTextChanged { text, _, _, _ ->
+        val valid = validators.all { condition -> condition(text?.toString() ?: "") }
+        setError(if (valid) null else error)
+        onValid(if (valid) text?.toString() ?: "" else "")
+    }
+}
+
+fun TextInputLayout.validate(validationObj: TextInputValidationObj, onValid: (String) -> Unit) {
+    setTag(R.id.validationTag, validationObj)
+    with(validationObj) {
+        validate(default, validators, error, onValid)
+    }
+}
+
+/**
+ * @return [TextInputValidationObj] else null
+ */
+fun TextInputLayout.getValidator() =
+    getTag(R.id.validationTag)?.let { it as TextInputValidationObj } ?: null
+
+/**
+ * @return [Boolean]
+ *  true if all validation conditions are met otherwise returns false
+ *  @throws [IllegalArgumentException] when validation object is not set
+ *  @See TextInputLayout.validate to set the validation object
+ */
+fun TextInputLayout.valid(): Boolean {
+    val validationObj = getValidator()
+    if (validationObj != null && editText != null) {
+        return validationObj.validators.all { condition ->
+            condition(
+                editText?.text?.toString() ?: ""
+            )
+        }.also { valid ->
+            error = if (valid) null else validationObj.error
+        }
+    } else {
+        throw IllegalArgumentException("Call TextInputLayout#validate(args) first $id")
+    }
+}
